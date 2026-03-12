44fcd1ea68a02c4e7ccc2c85ffc6b71e59f79f6d
Bilal1Ahmad <mansooribilalahmad@gmail.com>
2026-03-12 15:56:51 +0530

    Day 2: Update rooms route

--- app.py
+++ app.py
@@ -2,9 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Sample room data
+rooms = [
+    {"number": 101, "status": "Available"},
+    {"number": 102, "status": "Occupied"},
+    {"number": 103, "status": "Available"},
+    {"number": 104, "status": "Available"}
+]
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/rooms")
+def show_rooms():
+    return render_template("rooms.html", rooms=rooms)
 
 if __name__ == "__main__":
     app.run(debug=True)
