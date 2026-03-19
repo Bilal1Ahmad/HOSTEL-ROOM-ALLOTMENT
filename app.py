bed1208a491147aa75872c426c5933a74a207648
Bilal1Ahmad <mansooribilalahmad@gmail.com>
2026-03-19 22:51:05 +0530

    Updated app.py logic

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import sqlite3
 
 app = Flask(__name__)
+
+# Temporary student (for now)
+current_student = "Bilal"
 
 @app.route("/")
 def home():
@@ -19,6 +22,34 @@ def show_rooms():
     conn.close()
 
     return render_template("rooms.html", rooms=rooms)
+
+
+@app.route("/select/<int:room_id>")
+def select_room(room_id):
+
+    conn = sqlite3.connect("hostel.db")
+    cursor = conn.cursor()
+
+    # Check if student already has room
+    cursor.execute("SELECT * FROM rooms WHERE student=?", (current_student,))
+    existing = cursor.fetchone()
+
+    if existing:
+        conn.close()
+        return "You already selected a room!"
+
+    # Assign room
+    cursor.execute("""
+    UPDATE rooms
+    SET status='Occupied', student=?
+    WHERE id=?
+    """, (current_student, room_id))
+
+    conn.commit()
+    conn.close()
+
+    return redirect("/rooms")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
